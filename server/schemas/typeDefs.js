85ec739867691f51a9ecf1adf5c6ccb87958e756
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-07-27 12:32:11 -0700

    created typeDefs.js and updated resolver.js with new me query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Profile {
-    _id: ID
-    name: String
+  type User {
+    _id: ID!
+    username: String
     email: String
     password: String
-    skills: [String]!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
 
   type Auth {
     token: ID!
-    profile: Profile
+    user: User
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-    me: Profile
+    me: User
+  }
+
+  input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
   }
 
   type Mutation {
-    addProfile(name: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
 
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
+    saveBook(input: savedBook!): User
+    removeBook(bookId: String!): User
   }
 `;
 
